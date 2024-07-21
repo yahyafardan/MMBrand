79bfe1ff2cb6dbe1b07353b3ebe1eed7c1113789
yahyafardan <yahyaxgamer.00@gmail.com>
2024-07-21 17:35:44 +0300

    added pages

---
+++ design.php
@@ -0,0 +1,19 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+
+// Check if the user is an admin
+if ($_SESSION['role_name'] !== 'design') {
+    echo "Access denied.";
+    exit;
+}
+
+// Page content for admins
+echo "Welcome, " . $_SESSION['username'] . "! This is the designer page.";
+?>
